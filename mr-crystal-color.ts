fb8f5d3593d2146dcec38e8f2afee3912715d031
masanoni <jenseits.von.gut@gmail.com>
2025-07-14 16:45:47 +0900

    Add files via upload

---
+++ mr-crystal-color.ts
@@ -0,0 +1,100 @@
+import { Paint, Brand, PaintType, PaintSystem } from '../../../types';
+
+export const mrCrystalColorPaints: Paint[] = [
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "ダイヤモンドシルバー",
+    "code": "XC01",
+    "hex": "#e0e0e0",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/3IqjTPC",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "トパーズゴールド",
+    "code": "XC02",
+    "hex": "#e6c88e",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/40g8kAG",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "ルビーレッド",
+    "code": "XC03",
+    "hex": "#d94e6c",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/4515O3V",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "アメジストパープル",
+    "code": "XC04",
+    "hex": "#9f74c5",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/4lKrasB",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "サファイアブルー",
+    "code": "XC05",
+    "hex": "#6998cb",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/4nRSuqc",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "トルマリングリーン",
+    "code": "XC06",
+    "hex": "#67b4a3",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/44s97RB",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "ターコイズグリーン",
+    "code": "N/A",
+    "hex": "#00B3A3",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "https://amzn.to/3IpLMaC",
+    "rakutenUrl": ""
+  },
+  {
+    "brand": "GSIクレオス",
+    "series": "Mr.クリスタルカラー",
+    "name": "ムーンストーンパール",
+    "code": "N/A",
+    "hex": "#F8F8F8",
+    "type": "パール",
+    "paintSystem": "ラッカー",
+    "finish": "gloss",
+    "amazonUrl": "",
+    "rakutenUrl": ""
+  }
+];
